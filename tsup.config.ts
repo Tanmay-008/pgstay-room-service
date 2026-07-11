d68677de8ba926e51944536b476cd34132646918
Tanmay-008 <tanmayshirbhayye60@gmail.com>
2026-07-11 17:39:48 +0530

    add basic setup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    target: 'node22',
+    clean: true,
+    minify: true,
+    splitting: false,
+    sourcemap: true,
+    shims: true,
+});
